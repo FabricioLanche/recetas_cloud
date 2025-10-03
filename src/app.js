0f1e7bee5dca68994415b621d20dcf3272f907ab
FabricioLanche <fabricio.lanche@utec.edu.pe>
2025-10-03 03:10:13 -0500

    refactor(app): reorganize backend structure and move services, models, and configs

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const dbConfig = require('./config/dbConfig');
 const medicosController = require('./controllers/medicosController');
 const swaggerUi = require('swagger-ui-express');
 const openapi = require('./docs/openapi.json');
-const recetasController = require('./controllers/recetasController'); // <-- ESTA ES LA CLAVE
+const recetasController = require('./controllers/recetasController');
+const Receta = require('./models/recetaModel');
+const Medico = require('./models/medicosModel');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,14 +19,27 @@ app.use(express.urlencoded({ extended: true }));
 // Database connection
 console.log('[APP] dbConfig.url =', dbConfig.url);
 console.log('[APP] dbConfig.options =', dbConfig.options);
+
 mongoose.connect(dbConfig.url, dbConfig.options)
-    .then(() => {
+    .then(async () => {
         console.log('MongoDB connected successfully');
+
+        const collections = await mongoose.connection.db.listCollections().toArray();
+        const collectionNames = collections.map(c => c.name);
+
+        if (!collectionNames.includes(Receta.collection.name)) {
+            await mongoose.connection.createCollection(Receta.collection.name);
+            console.log('[DB] Colección "recetas" creada');
+        }
+
+        if (!collectionNames.includes(Medico.collection.name)) {
+            await mongoose.connection.createCollection(Medico.collection.name);
+            console.log('[DB] Colección "medicos" creada');
+        }
     })
     .catch(err => {
         console.error('MongoDB connection error:', err);
     });
-
 // Documentación OpenAPI (Swagger) en JSON
 app.use('/api/recetas/docs', swaggerUi.serve, swaggerUi.setup(openapi));
 
